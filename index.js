1143d9a03d7bded743c80c670a22ac2d49e7335f
soohoo05 <tyler.soohoo@gmail.com>
2018-10-31 11:32:25 -0400

    done

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 $('#save_lincoln').click(()=>saveLincoln(parseInt($('#interval').val())))
+let lincoln=document.getElementById('save_lincoln')
+let inter=document.getElementById('interval').value
 
+lincoln.addEventListener('click',()=>saveLincoln(inter))
 function saveLincoln(interval) {
+  
   if(isNaN(interval)) {
     interval = 10000
   }
